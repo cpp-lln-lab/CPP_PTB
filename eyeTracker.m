66600be4ca6b21277b8ff1c8ac6b4a241b6076d0
marcobarilari <marcobarilr@gmail.com>
2020-07-17 19:25:23 +0200

    add tested and working version

--- eyeTracker.m
+++ eyeTracker.m
@@ -1,4 +1,4 @@
-function [ el, edfFile ] = eyeTracker(input, cfg, expParameters)
+function [ el, edfFile ] = eyeTracker(input, cfg, expParameters, varargin)
 
 if ~cfg.eyeTracker
 
@@ -6,67 +6,20 @@ if ~cfg.eyeTracker
 
 else
 
-    %% Assign parameters
-    edfFile = expParameters.fileName.eyetracker;
-
     switch input
 
         case 'Calibration'
-%%
-% screenNumber = max(Screen('Screens'));
-% gray=GrayIndex(screenNumber);
-% [w, wRect]=Screen('OpenWindow',screenNumber, gray);
-% Screen('TextSize', w, 32);
-% silver=[192 192 192,(w)];
-% dummymode=0;
-%
-% el=EyelinkInitDefaults(w);
-% el.backgroundcolour = silver;
-% el.msgfontcolour    = BlackIndex(w);
-% el.calibrationtargetcolour= BlackIndex(w);
-% el.calibrationtargetsize= 1;
-% el.calibrationtargetwidth=0.5;
-% el.displayCalResults = 1;
-% EyelinkUpdateDefaults(el);
-%
-% if ~EyelinkInit(dummymode, 1)
-% fprintf('Eyelink Init aborted.\n');
-% cleanup;  % cleanup function
-% return;
-% end
-%
-% [~, vs] = Eyelink('GetTrackerVersion');
-% fprintf('Running experiment on a ''%s'' tracker.\n', vs );
-% Eyelink('Command', 'link_sample_data = LEFT,RIGHT,GAZE,AREA');
-%
-% edfFile='demo.edf';
-% Eyelink('Openfile', edfFile);
-%
-% Eyelink('command','screen_pixel_coords = %ld %ld %ld %ld', 0, 0, 0, 0);
-% Eyelink('message', 'DISPLAY_COORDS %ld %ld %ld %ld', 0, 0, 0, 0);
-%
-% Eyelink('command', 'calibration_type = HV5');
-% Eyelink('command', 'generate_default_targets = YES');
-%
-% fprintf('Running experiment on a ''%s'' tracker.\n', vs );
-% vsn = regexp(vs,'\d','match');
-% EyelinkDoTrackerSetup(el);
-%
-% %go back to black screen
-% Screen('FillRect', w, [0 0 0]);
-% blank_onset=Screen('Flip', w);
 
             %% STEP 2
             % Provide Eyelink with details about the graphics environment
             %  and perform some initializations. The information is returned
             %  in a structure that also contains useful defaults
             %  and control codes (e.g. tracker state bit and Eyelink key values).
-
             el = EyelinkInitDefaults(cfg.win);
 
             % calibration has silver background with black targets, sound and smaller
             %  targets
-            el.backgroundcolour        = [192 192 192,(cfg.win)];
+            el.backgroundcolour        = [ 192 192 192, (cfg.win)];
             el.msgfontcolour           = BlackIndex(cfg.win);
             el.calibrationtargetcolour = BlackIndex(cfg.win);
             el.calibrationtargetsize   = 1;
@@ -99,30 +52,30 @@ else
                 return;
             end
 
+             %
+            if ~EyelinkInit(0, 1)
+                fprintf('Eyelink Init aborted.\n');
+                return;
+            end
+
+            % Open the edf file to write the data
+            edfFile = 'demo.edf';
+            Eyelink('Openfile', edfFile);
+
             [el.v, el.vs] = Eyelink('GetTrackerVersion');
             fprintf('Running experiment on a ''%s'' tracker.\n', el.vs );
 
             % make sure that we get gaze data from the Eyelink
             Eyelink('Command', 'link_sample_data = LEFT,RIGHT,GAZE,AREA');
 
-%             % open file to record data to
-%             if ~exist('eyetracker','dir')
-%                 mkdir('eyetracker')
-%             end
-
-            fileEDF = 'demo.edf';
-            Eyelink('Openfile', fileEDF);
-
             % STEP 4
             % SET UP TRACKER CONFIGURATION
             % Setting the proper recording resolution, proper calibration type,
             %   as well as the data file content;
-%            Eyelink('command', 'add_file_preamble_text ''Recorded by EyelinkToolbox demo-experiment''');
+            %            Eyelink('command', 'add_file_preamble_text ''Recorded by EyelinkToolbox demo-experiment''');
 
             % This command is crucial to map the gaze positions from the tracker to
             %  screen pixel positions to determine fixation
-%            Eyelink('command','screen_pixel_coords = %ld %ld %ld %ld', 0, 0, cfg.winWidth-1, cfg.winHeight-1);
-%            Eyelink('message', 'DISPLAY_COORDS %ld %ld %ld %ld', 0, 0, cfg.winWidth-1, cfg.winHeight-1);
             Eyelink('command','screen_pixel_coords = %ld %ld %ld %ld', 0, 0, 0, 0);
             Eyelink('message', 'DISPLAY_COORDS %ld %ld %ld %ld', 0, 0, 0, 0);
 
@@ -136,38 +89,38 @@ else
             Eyelink('command', 'generate_default_targets = YES');
             % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
 
-%         % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
-%         % CUSTOM CALIBRATION
-%         % (SET MANUALLY THE DOTS COORDINATES, HERE FOR 6 DOTS)
-%         Eyelink('command', 'calibration_type = HV5');
-%         % you must send this command with value NO for custom calibration
-%         % you must also reset it to YES for subsequent experiments
-%         Eyelink('command', 'generate_default_targets = NO');
-%
-%         % calibration and validation target locations
-%         [width, height]=Screen('WindowSize', screenNumber);
-%         Eyelink('command','calibration_samples = 6');
-%         Eyelink('command','calibration_sequence = 0,1,2,3,4,5');
-%         Eyelink('command','calibration_targets = %d,%d %d,%d %d,%d %d,%d %d,%d',...
-%             640,512, ... %width/2,height/2
-%             640,102, ... %width/2,height*0.1
-%             640,614, ... %width/2,height*0.6
-%             128,341, ... %width*0.1,height*1/3
-%             1152,341 );  %width-width*0.1,height*1/3
-%
-%         Eyelink('command','validation_samples = 5');
-%         Eyelink('command','validation_sequence = 0,1,2,3,4,5');
-%         Eyelink('command','validation_targets = %d,%d %d,%d %d,%d %d,%d %d,%d',...
-%             640,512, ... %width/2,height/2
-%             640,102, ... %width/2,height*0.1
-%             640,614, ... %width/2,height*0.6
-%             128,341, ... %width*0.1,height*1/3
-%             1152,341 );  %width-width*0.1,height*1/3
-%         % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+            %         % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+            %         % CUSTOM CALIBRATION
+            %         % (SET MANUALLY THE DOTS COORDINATES, HERE FOR 6 DOTS)
+            %         Eyelink('command', 'calibration_type = HV5');
+            %         % you must send this command with value NO for custom calibration
+            %         % you must also reset it to YES for subsequent experiments
+            %         Eyelink('command', 'generate_default_targets = NO');
+            %
+            %         % calibration and validation target locations
+            %         [width, height]=Screen('WindowSize', screenNumber);
+            %         Eyelink('command','calibration_samples = 6');
+            %         Eyelink('command','calibration_sequence = 0,1,2,3,4,5');
+            %         Eyelink('command','calibration_targets = %d,%d %d,%d %d,%d %d,%d %d,%d',...
+            %             640,512, ... %width/2,height/2
+            %             640,102, ... %width/2,height*0.1
+            %             640,614, ... %width/2,height*0.6
+            %             128,341, ... %width*0.1,height*1/3
+            %             1152,341 );  %width-width*0.1,height*1/3
+            %
+            %         Eyelink('command','validation_samples = 5');
+            %         Eyelink('command','validation_sequence = 0,1,2,3,4,5');
+            %         Eyelink('command','validation_targets = %d,%d %d,%d %d,%d %d,%d %d,%d',...
+            %             640,512, ... %width/2,height/2
+            %             640,102, ... %width/2,height*0.1
+            %             640,614, ... %width/2,height*0.6
+            %             128,341, ... %width*0.1,height*1/3
+            %             1152,341 );  %width-width*0.1,height*1/3
+            %         % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
 
-%             % set parser (conservative saccade thresholds)
-%             Eyelink('command', 'saccade_velocity_threshold = 35');
-%             Eyelink('command', 'saccade_acceleration_threshold = 9500');
+            %             % set parser (conservative saccade thresholds)
+            %             Eyelink('command', 'saccade_velocity_threshold = 35');
+            %             Eyelink('command', 'saccade_acceleration_threshold = 9500');
 
             % set EDF file contents (not clear what this lines are used for)
             el.vsn = regexp(el.vs,'\d','match'); % wont work on EL
@@ -175,17 +128,21 @@ else
             % enter Eyetracker camera setup mode, calibration and validation
             EyelinkDoTrackerSetup(el);
 
-%             %         % do a final check of calibration using driftcorrection
-%             %         % You have to hit esc before return.
-%             %         EyelinkDoDriftCorrection(el);
-%
-%             %         % do a final check of calibration using driftcorrection
-%             %         success=EyelinkDoDriftCorrection(el);
-%             %         if success~=1
-%             %             Eyelink('shutdown');
-%             %             Screen('CloseAll');
-%             %             return;
-%             %         end
+            %             %         % do a final check of calibration using driftcorrection
+            %             %         % You have to hit esc before return.
+            %             %         EyelinkDoDriftCorrection(el);
+            %
+            %             %         % do a final check of calibration using driftcorrection
+            %             %         success=EyelinkDoDriftCorrection(el);
+            %             %         if success~=1
+            %             %             Eyelink('shutdown');
+            %             %             Screen('CloseAll');
+            %             %             return;
+            %             %         end
+
+            % Go back to black screen
+            Screen('FillRect', cfg.win, [0 0 0]);
+            Screen('Flip', cfg.win);
 
         case 'StartRecording'
 
@@ -228,6 +185,9 @@ else
 
         case 'Shutdown'
 
+            edfFileName = expParameters.fileName.eyetracker;
+            edfFile = 'demo.edf';
+
             % STEP 6
             % At the end of the experiment, save the edf file and shut down connection
             %  with Eyelink
@@ -238,16 +198,16 @@ else
 
             % download data file
             try
-                fprintf('Receiving data file ''%s''\n', fileEdf );
-                status=Eyelink('ReceiveFile','',fileEdf);
+                fprintf('Receiving data file ''%s''\n', edfFileName );
+                status=Eyelink('ReceiveFile','',[expParameters.outputDir, filesep, 'eyetracker', filesep, edfFileName]);
                 if status > 0
                     fprintf('ReceiveFile status %d\n', status);
                 end
-                if 2==exist(fileEdf, 'file')
-                    fprintf('Data file ''%s'' can be found in ''%s''\n', fileEdf, pwd );
+                if 2==exist([expParameters.outputDir, filesep, 'eyetracker', filesep, edfFileName], 'file')
+                    fprintf('Data file ''%s'' can be found in ''%s''\n', edfFileName, [expParameters.outputDir, filesep, 'eyetracker', filesep] );
                 end
             catch
-                fprintf('Problem receiving data file ''%s''\n', fileEdf );
+                fprintf('Problem receiving data file ''%s''\n', edfFileName );
             end
 
             Eyelink('shutdown');
