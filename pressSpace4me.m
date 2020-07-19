81412fe2ab9c8939f0e7d2819794b684192cc6dd
Remi Gau <remi_gau@hotmail.com>
2020-07-19 11:22:01 +0200

    make press space bar listen to all devices by defaults

--- pressSpace4me.m
+++ pressSpace4me.m
@@ -1,13 +1,19 @@
-function pressSpace4me
+function pressSpace4me(deviceNumber)
 % Use that to stop your script and only restart when the space bar is pressed.
+% When no deviceNumber is set then it will check the default device
+   
+if nargin < 1 || isempty(deviceNumber)
+    deviceNumber = -1;
+end
 
 fprintf('\nPress space to continue.\n');
 
 while 1
 
+    % check keyboard very 100 ms
     WaitSecs(0.1);
 
-    [~, keyCode, ~] = KbWait(-1);
+    [~, keyCode, ~] = KbWait(deviceNumber);
 
     if strcmp(KbName(find(keyCode)), 'space')
         fprintf('starting the experiment...\n');
