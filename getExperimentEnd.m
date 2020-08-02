fbd03dba41fbef10200f045de5c961593f3fe1f4
Remi Gau <remi_gau@hotmail.com>
2020-08-02 11:43:23 +0200

    add wrapper functions for experiment start end farewell and stand by screen

---
+++ getExperimentEnd.m
@@ -0,0 +1,15 @@
+function cfg = getExperimentEnd(cfg)
+    
+    drawFixation(cfg);
+    endExpmt = Screen('Flip', cfg.screen.win);
+    
+    disp(' ');
+    ExpmtDur = endExpmt - cfg.experimentStart;
+    ExpmtDurMin = floor(ExpmtDur / 60);
+    ExpmtDurSec = mod(ExpmtDur, 60);
+    disp(['Experiment lasted ', ...
+        num2str(ExpmtDurMin), ' minutes', ...
+        num2str(ExpmtDurSec), ' seconds']);
+    disp(' ');
+    
+end
---
+++ standByScreen.m
@@ -0,0 +1,14 @@
+function standByScreen(cfg)
+    
+    Screen('FillRect', cfg.screen.win, cfg.color.background, cfg.screen.winRect);
+
+    DrawFormattedText(cfg.screen.win, ...
+        cfg.task.instruction, ...
+        'center', 'center', cfg.text.color);
+
+    Screen('Flip', cfg.screen.win);
+    
+    % Wait for space key to be pressed
+    pressSpaceForMe();
+    
+end
