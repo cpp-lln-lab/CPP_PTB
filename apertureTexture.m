26de68f9f3e1bd6479bec98b5cb83a9871c76308
Remi Gau <remi_gau@hotmail.com>
2020-08-02 13:16:08 +0200

    update from visual localizer to use textures

--- apertureTexture.m
+++ apertureTexture.m
@@ -1,12 +1,18 @@
-
 function cfg = apertureTexture(action, cfg, thisEvent)
     
-    matrixSize = 400;
+    transparent = [0 0 0 0];
     
     switch action
         
         case 'init'
             
+            % we take the screen height as maximum aperture width if not
+            % specified.
+            if ~isfield(cfg.aperture, 'width') || isempty(cfg.aperture.width)
+                cfg.aperture.width = cfg.screen.winRect(4) / cfg.screen.ppd;
+            end
+            cfg.aperture = degToPix('width', cfg.aperture, cfg);
+
             cfg.aperture.texture = Screen('MakeTexture', cfg.screen.win, ...
                 cfg.color.background(1) * ones(cfg.screen.winRect([4 3])));
             
@@ -16,12 +22,14 @@ function cfg = apertureTexture(action, cfg, thisEvent)
                 
                 case 'none'
                     
-                    Screen('Fillrect', cfg.aperture.texture, [0 0 0 0]);
+                    Screen('Fillrect', cfg.aperture.texture, transparent);
                       
                 case 'circle'
 
-                    Screen('FillOval', cfg.aperture.texture, [0 0 0 0], ...
-                        CenterRectOnPoint([0 0 repmat(matrixSize, 1, 2)], ...
+                    diameter = cfg.aperture.widthPix;
+
+                    Screen('FillOval', cfg.aperture.texture, transparent, ...
+                        CenterRectOnPoint([0 0 repmat(diameter, 1, 2)], ...
                         cfg.screen.winRect(3) / 2, cfg.screen.winRect(4) / 2));
 
             end
@@ -30,15 +38,9 @@ function cfg = apertureTexture(action, cfg, thisEvent)
             
             Screen('DrawTexture', cfg.screen.win, cfg.aperture.texture);
             
-            %         Screen('DrawTexture', cfg.screen.win, apertureTexture, ...
-            %             cfg.screen.winRect, cfg.screen.winRect, current.apertureAngle - 90);
-            
-            
+            % Screen('DrawTexture', cfg.screen.win, apertureTexture, ...
+            % cfg.screen.winRect, cfg.screen.winRect, current.apertureAngle - 90);
+                        
     end
-    
-    
-    
-    
-    
-    
+
 end
