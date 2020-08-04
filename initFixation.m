867aba4f2b2526946f95a2b43051159451a0009e
Remi Gau <remi_gau@hotmail.com>
2020-08-04 15:53:30 +0200

    refactor fixation from retinotopy

--- initFixation.m
+++ initFixation.m
@@ -1,11 +1,11 @@
 function cfg = initFixation(cfg)
+    
+    % Convert some values from degrees to pixels
+    cfg.fixation = degToPix('width', cfg.fixation, cfg);
+    cfg.fixation = degToPix('xDisplacement', cfg.fixation, cfg);
+    cfg.fixation = degToPix('yDisplacement', cfg.fixation, cfg);
 
     if strcmp(cfg.fixation.type, 'cross')
-
-        % Convert some values from degrees to pixels
-        cfg.fixation = degToPix('width', cfg.fixation, cfg);
-        cfg.fixation = degToPix('xDisplacement', cfg.fixation, cfg);
-        cfg.fixation = degToPix('yDisplacement', cfg.fixation, cfg);
 
         % Prepare fixation cross
         cfg.fixation.xCoords = [-cfg.fixation.widthPix cfg.fixation.widthPix 0 0] / 2 + ...
