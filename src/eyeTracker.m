59887265849c1740218182599937e415674504b5
marcobarilari <38101692+marcobarilari@users.noreply.github.com>
2020-08-18 18:52:54 +0200

    Update src/eyeTracker.m
    Co-authored-by: Remi Gau <remi_gau@hotmail.com>

--- src/eyeTracker.m
+++ src/eyeTracker.m
@@ -90,7 +90,7 @@ function [el] = eyeTracker(input, cfg)
                 % Make sure that we get gaze data from the Eyelink.
                 Eyelink('Command', 'link_sample_data = LEFT,RIGHT,GAZE,AREA');
                 Eyelink('Command', 'link_event_data = GAZE,GAZERES,HREF,AREA,VELOCITY');
-                Eyelink('command', 'link_event_filter = LEFT,RIGHT,FIXATION,BLINK,SACCADE,BUTTON');
+                Eyelink('Command', 'link_event_filter = LEFT,RIGHT,FIXATION,BLINK,SACCADE,BUTTON');
 
                 %% Calibration
 
