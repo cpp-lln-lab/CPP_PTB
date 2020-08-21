f4d6521836a68a51f8d4563085d2192d33a81e07
marcobarilari <marcobarilr@gmail.com>
2020-08-21 12:42:43 +0200

    fix Travis' suggestions

--- src/readAndFilterLogfile.m
+++ src/readAndFilterLogfile.m
@@ -9,30 +9,30 @@ function outputFiltered = readAndFilterLogfile(columnName, filterBy, varargin)
     %
     % INPUT:
     %
-    %  - columnName: string, the header of the column where the content of insterest is stored 
+    %  - columnName: string, the header of the column where the content of insterest is stored
     %    (e.g., for 'trigger' will be 'trial type')
     %  - filterBy: string, the content of the column you want to filter out. It can take just
     %    part of the content name (e.g., you want to display the triggers and you have
     %    'trigger_motion' and 'trigger_static', 'trigger' as input will do)
-    %  - varargin: either cfg (to display the last run output) or the file path as string   
+    %  - varargin: either cfg (to display the last run output) or the file path as string
     %
     % OUTPUT:
     %
     %  - outputFiltered: dataset with only the specified content, to see it in the command window
     %  use display(outputFiltered)
-    
+
     % Checke if input is cfg or the file path
     if ischar(varargin{1})
-       tsvFile = varargin{1};
+        tsvFile = varargin{1};
     elseif isstruct(varargin{1})
-       tsvFile = fullfile(varargin{1}.dir.outputSubject, ...
-            varargin{1}.fileName.modality, ...
-            varargin{1}.fileName.events);
+        tsvFile = fullfile(varargin{1}.dir.outputSubject, ...
+             varargin{1}.fileName.modality, ...
+             varargin{1}.fileName.events);
     end
 
     % Check if the file exists
     if ~exist(tsvFile, 'file')
-      error([newline 'Input file does not exist'])
+        error([newline 'Input file does not exist']);
     end
 
     % Read the the tsv file and store each column in a field of `output` structure
