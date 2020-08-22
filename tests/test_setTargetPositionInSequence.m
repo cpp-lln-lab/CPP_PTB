d1e40b738a116ecd385457a2047e5b49ce05e83c
Remi Gau <remi_gau@hotmail.com>
2020-08-22 10:25:54 +0200

    add initEnv as subfunction to unit test

--- tests/test_setTargetPositionInSequence.m
+++ tests/test_setTargetPositionInSequence.m
@@ -1,8 +1,6 @@
 function test_suite = test_setTargetPositionInSequence %#ok<*STOUT>
     
-    if isOctave()
-        pkg('load','statistics');
-    end
+    initEnv()
     
     try % assignment of 'localfunctions' is necessary in Matlab >= 2016
         test_functions = localfunctions(); %#ok<*NASGU>
@@ -28,5 +26,52 @@ function test_setTargetPositionInSequenceBasic()
 
     interval = abs(diff(chosenPositions, [], 2));
     assertTrue(all(interval(:) > 1));
+
+end
+
+function initEnv
+
+    octaveVersion = '4.0.3';
+    matlabVersion = '8.6.0';
+    
+    installlist = {'statistics', 'image'};
+
+    if isOctave
+
+        % Exit if min version is not satisfied
+        if ~compare_versions(OCTAVE_VERSION, octaveVersion, '>=')
+            error('Minimum required Octave version: %s', octaveVersion);
+        end
+
+        for ii = 1:length(installlist)
+            try
+                % Try loading Octave packages
+                disp(['loading ' installlist{ii}]);
+                pkg('load', installlist{ii});
+
+            catch
+                errorcount = 1;
+                while errorcount % Attempt twice in case installation fails
+                    try
+                        pkg('install', '-forge', installlist{ii});
+                        pkg('load', installlist{ii});
+                        errorcount = 0;
+                    catch err
+                        errorcount = errorcount + 1;
+                        if errorcount > 2
+                            error(err.message);
+                        end
+                    end
+                end
+            end
+        end
+
+    else % MATLAB ----------------------------
+
+        if verLessThan('matlab', matlabVersion)
+            error('Sorry, minimum required version is R2017b. :(');
+        end
+
+    end
 
 end
