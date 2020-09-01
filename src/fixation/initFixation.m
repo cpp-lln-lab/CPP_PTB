3e886afdfe760ea72c1240e9f0b6f8c1015b816c
Remi Gau <remi_gau@hotmail.com>
2020-09-01 17:45:04 +0200

    write test for initFixation

--- src/fixation/initFixation.m
+++ src/fixation/initFixation.m
@@ -1,39 +1,58 @@
 function cfg = initFixation(cfg)
-
+    % cfg = initFixation(cfg)
+    %
+    % prepare details for fixation "cross"
+    %
+    % the fixation has a width defined by
+    % cfg.fixation.width : in degrees of visual
+    %
+    % The horizontal and vertical offset (in degrees of visual) with respect to the center of the
+    % screen is defined by
+    % cfg.fixation.xDisplacement
+    % cfg.fixation.yDisplacement
+    %
+    % for cfg.fixation.type == 'bestFixation'            
+    % Code adapted from:
+    % "What is the best fixation target?"
+    % DOI 10.1016/j.visres.2012.10.012
+    %
+    
+    
     % Convert some values from degrees to pixels
     cfg.fixation = degToPix('width', cfg.fixation, cfg);
     cfg.fixation = degToPix('xDisplacement', cfg.fixation, cfg);
     cfg.fixation = degToPix('yDisplacement', cfg.fixation, cfg);
-
+    
     % Prepare fixation cross
     xLine = [-cfg.fixation.widthPix cfg.fixation.widthPix 0 0] / 2;
     yLine = [0 0 -cfg.fixation.widthPix cfg.fixation.widthPix] / 2;
-
+    
     cfg.fixation.xCoords =  xLine + cfg.fixation.xDisplacementPix;
     cfg.fixation.yCoords =  yLine + cfg.fixation.yDisplacementPix;
-
+    
     cfg.fixation.allCoords = [cfg.fixation.xCoords; cfg.fixation.yCoords];
-
+    
+    
     switch cfg.fixation.type
-
+        
         case 'bestFixation'
-
+            
             % Code adapted from:
             % What is the best fixation target?
             % DOI 10.1016/j.visres.2012.10.012
-
+            
             cfg.fixation.outerOval = [ ...
                 cfg.screen.center(1) - cfg.fixation.widthPix / 2, ...
                 cfg.screen.center(2) - cfg.fixation.widthPix / 2, ...
                 cfg.screen.center(1) + cfg.fixation.widthPix / 2, ...
                 cfg.screen.center(2) + cfg.fixation.widthPix / 2];
-
+            
             cfg.fixation.innerOval = [ ...
                 cfg.screen.center(1) - cfg.fixation.widthPix / 6, ...
                 cfg.screen.center(2) - cfg.fixation.widthPix / 6, ...
                 cfg.screen.center(1) + cfg.fixation.widthPix / 6, ...
                 cfg.screen.center(2) + cfg.fixation.widthPix / 6];
-
+            
     end
-
+    
 end
