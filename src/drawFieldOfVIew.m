5d596483bb744f5e6614a7dcc8243ce5103042e2
Remi Gau <remi_gau@hotmail.com>
2020-09-08 16:59:32 +0200

    add a draw field of view function

---
+++ src/drawFieldOfVIew.m
@@ -0,0 +1,25 @@
+function drawFieldOfVIew(cfg)
+    % drawFieldOfVIew(cfg)
+    %
+    % draws a red rectangle on the screen to materialize the field of view of
+    % the participant. This can be used during debugging to help design the
+    % stimuli if you know the FOV of the participant will be obstructed by
+    % something
+    
+    RED = [255 0 0];
+    penWidth = 2;
+    
+    fov = cfg.screen.effectiveFieldOfView;
+    
+    fov = CenterRect(...
+        [0, 0, fov(1), fov(2)], ...
+        cfg.screen.winRect);
+
+    Screen('FrameRect', ...
+        cfg.screen.win, ...
+        RED, ...
+        fov, ...
+        penWidth);
+    
+    
+end
