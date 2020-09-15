47fd27f5bb42773578ea7ecb51135465b29e08bf
Remi Gau <remi_gau@hotmail.com>
2020-09-15 16:48:30 +0200

    refactor aperture texture

--- src/aperture/apertureTexture.m
+++ src/aperture/apertureTexture.m
@@ -14,108 +14,7 @@ function [cfg, thisEvent] = apertureTexture(action, cfg, thisEvent)
 
         case 'make'
 
-            TRANSPARENT = [0, 0, 0, 0];
-
-            xCenter = cfg.screen.center(1);
-            yCenter = cfg.screen.center(2);
-
-            switch cfg.aperture.type
-
-                case 'none'
-
-                    Screen('Fillrect', cfg.aperture.texture, TRANSPARENT);
-
-                case 'circle'
-
-                    diameter = cfg.aperture.widthPix;
-
-                    if isfield(cfg.aperture, 'xPosPix')
-                        xCenter = cfg.screen.center(1) + cfg.aperture.xPosPix;
-                    end
-                    if isfield(cfg.aperture, 'yPosPix')
-                        yCenter = cfg.screen.center(2) + cfg.aperture.yPosPix;
-                    end
-
-                    Screen('FillOval', cfg.aperture.texture, TRANSPARENT, ...
-                        CenterRectOnPoint([0, 0, repmat(diameter, 1, 2)], ...
-                        xCenter, yCenter));
-
-                case 'ring'
-
-                    % expansion speed is log over eccentricity
-                    [cfg] = eccenLogSpeed(cfg, thisEvent.time);
-
-                    Screen('Fillrect', cfg.aperture.texture, cfg.color.background);
-
-                    Screen('FillOval', cfg.aperture.texture, TRANSPARENT, ...
-                        CenterRectOnPoint( ...
-                        [0, 0, repmat(cfg.ring.outerRimPix, 1, 2)], ...
-                        xCenter, yCenter));
-
-                    Screen('FillOval', cfg.aperture.texture, [cfg.color.background 255], ...
-                        CenterRectOnPoint( ...
-                        [0, 0, repmat(cfg.ring.innerRimPix, 1, 2)], ...
-                        xCenter, yCenter));
-
-                case 'wedge'
-
-                    cycleDuration = cfg.mri.repetitionTime * cfg.volsPerCycle;
-
-                    % Update angle for rotation of background and for apperture for wedge
-                    switch cfg.direction
-
-                        case '+'
-                            thisEvent.angle = 90 - ...
-                                cfg.aperture.width / 2 + ...
-                                (thisEvent.time / cycleDuration) * 360;
-                        case '-'
-                            thisEvent.angle = 90 - ...
-                                cfg.aperture.width / 2 - ...
-                                (thisEvent.time / cycleDuration) * 360;
-
-                    end
-
-                    Screen('Fillrect', cfg.aperture.texture, cfg.color.background);
-
-                    Screen('FillArc', cfg.aperture.texture, TRANSPARENT, ...
-                        CenterRect( ...
-                        cfg.destinationRect, ...
-                        cfg.screen.winRect), ...
-                        thisEvent.angle, ... % start angle
-                        cfg.aperture.width); % arc angle
-
-                case 'bar'
-
-                    % aperture is the color of the background
-                    Screen('FillRect', cfg.aperture.texture, cfg.color.background);
-
-                    % We let the stimulus through
-                    Screen('FillOval', cfg.aperture.texture, TRANSPARENT, ...
-                        CenterRect( ...
-                        [0, 0, repmat(cfg.screen.winRect(4), 1, 2)], ...
-                        cfg.screen.winRect));
-
-                    % Then we add the position of the bar aperture
-
-                    % which one is the right and which one is the left??
-
-                    Screen('FillRect', cfg.aperture.texture, cfg.color.background, ...
-                        [0, ...
-                        0, ...
-                        thisEvent.barPosPix - cfg.aperture.barWidthPix / 2, ...
-                        cfg.screen.winRect(4)]);
-
-                    Screen('FillRect', cfg.aperture.texture, cfg.color.background, ...
-                        [thisEvent.barPosPix + cfg.aperture.barWidthPix / 2, ...
-                        0, ...
-                        cfg.screen.winRect(3), ...
-                        cfg.screen.winRect(4)]);
-
-                otherwise
-
-                    error('unknown aperture type: %s.', cfg.aperture.type);
-
-            end
+            cfg = apertureTextureMake(cfg, thisEvent);
 
         case 'draw'
 
@@ -186,6 +85,113 @@ function cfg = apertureInit(cfg)
             cfg.aperture.barPos = ...
                 (cfg.aperture.barPosPix - cfg.screen.center(1)) / ...
                 cfg.screen.ppd;
+
+    end
+
+end
+
+function cfg = apertureTextureMake(cfg, thisEvent)
+
+    TRANSPARENT = [0, 0, 0, 0];
+
+    xCenter = cfg.screen.center(1);
+    yCenter = cfg.screen.center(2);
+
+    switch cfg.aperture.type
+
+        case 'none'
+
+            Screen('Fillrect', cfg.aperture.texture, TRANSPARENT);
+
+        case 'circle'
+
+            diameter = cfg.aperture.widthPix;
+
+            if isfield(cfg.aperture, 'xPosPix')
+                xCenter = xCenter + cfg.aperture.xPosPix;
+            end
+            if isfield(cfg.aperture, 'yPosPix')
+                yCenter = yCenter + cfg.aperture.yPosPix;
+            end
+
+            Screen('FillOval', cfg.aperture.texture, TRANSPARENT, ...
+                CenterRectOnPoint([0, 0, repmat(diameter, 1, 2)], ...
+                xCenter, yCenter));
+
+        case 'ring'
+
+            % expansion speed is log over eccentricity
+            [cfg] = eccenLogSpeed(cfg, thisEvent.time);
+
+            Screen('Fillrect', cfg.aperture.texture, cfg.color.background);
+
+            Screen('FillOval', cfg.aperture.texture, TRANSPARENT, ...
+                CenterRectOnPoint( ...
+                [0, 0, repmat(cfg.ring.outerRimPix, 1, 2)], ...
+                xCenter, yCenter));
+
+            Screen('FillOval', cfg.aperture.texture, [cfg.color.background 255], ...
+                CenterRectOnPoint( ...
+                [0, 0, repmat(cfg.ring.innerRimPix, 1, 2)], ...
+                xCenter, yCenter));
+
+        case 'wedge'
+
+            cycleDuration = cfg.mri.repetitionTime * cfg.volsPerCycle;
+
+            % Update angle for rotation of background and for apperture for wedge
+            switch cfg.direction
+
+                case '+'
+                    thisEvent.angle = 90 - ...
+                        cfg.aperture.width / 2 + ...
+                        (thisEvent.time / cycleDuration) * 360;
+                case '-'
+                    thisEvent.angle = 90 - ...
+                        cfg.aperture.width / 2 - ...
+                        (thisEvent.time / cycleDuration) * 360;
+
+            end
+
+            Screen('Fillrect', cfg.aperture.texture, cfg.color.background);
+
+            Screen('FillArc', cfg.aperture.texture, TRANSPARENT, ...
+                CenterRect( ...
+                cfg.destinationRect, ...
+                cfg.screen.winRect), ...
+                thisEvent.angle, ... % start angle
+                cfg.aperture.width); % arc angle
+
+        case 'bar'
+
+            % aperture is the color of the background
+            Screen('FillRect', cfg.aperture.texture, cfg.color.background);
+
+            % We let the stimulus through
+            Screen('FillOval', cfg.aperture.texture, TRANSPARENT, ...
+                CenterRect( ...
+                [0, 0, repmat(cfg.screen.winRect(4), 1, 2)], ...
+                cfg.screen.winRect));
+
+            % Then we add the position of the bar aperture
+
+            % which one is the right and which one is the left??
+
+            Screen('FillRect', cfg.aperture.texture, cfg.color.background, ...
+                [0, ...
+                0, ...
+                thisEvent.barPosPix - cfg.aperture.barWidthPix / 2, ...
+                cfg.screen.winRect(4)]);
+
+            Screen('FillRect', cfg.aperture.texture, cfg.color.background, ...
+                [thisEvent.barPosPix + cfg.aperture.barWidthPix / 2, ...
+                0, ...
+                cfg.screen.winRect(3), ...
+                cfg.screen.winRect(4)]);
+
+        otherwise
+
+            error('unknown aperture type: %s.', cfg.aperture.type);
 
     end
 
