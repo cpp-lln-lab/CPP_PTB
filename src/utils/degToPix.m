2b20116a872c7682ecf00b941c2155c1567a9221
Remi Gau <remi_gau@hotmail.com>
2020-09-23 10:52:19 +0200

    add function for pix to degree conversion

--- src/utils/degToPix.m
+++ src/utils/degToPix.m
@@ -4,6 +4,19 @@ function structure = degToPix(fieldName, structure, cfg)
     % For a given field value in degrees of visual angle in the structure,
     % this computes its value in pixel using the pixel per degree value of the cfg structure
     % and returns a structure with an additional field with Pix suffix holding that new value.
+    %
+    %
+    % USAGE:
+    % ------
+    % fixation.width = 2;
+    % cfg.screen.ppd = 10;
+    % 
+    % fixation = degToPix('width', fixation, cfg);
+    %
+    % Returns:
+    % ------- 
+    % fixation.widthPix = 20;
+    %
 
     deg = getfield(structure, fieldName); %#ok<GFLD>
 
