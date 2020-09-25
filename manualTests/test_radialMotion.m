728967740549f8cd7319073d55932a5f3fc37d66
Remi Gau <remi_gau@hotmail.com>
2020-09-25 22:18:22 +0200

    MS / MST ready

--- manualTests/test_radialMotion.m
+++ manualTests/test_radialMotion.m
@@ -1,19 +1,18 @@
 % ensure that dog homogenity is not too low when we kill dots often enough
 
-nbEvents = 1;
+nbEvents = 10;
 doPlot = true;
 
-thisEvent.direction = -666; % degrees
+thisEvent.direction = 666; % degrees
 thisEvent.speed = 1; % pix per frame
 
 cfg.design.motionType = 'radial';
 
 cfg.dot.coherence = 1; % proportion
 cfg.dot.lifeTime = Inf; % in seconds
-cfg.dot.matrixWidth = 1000; % in pixels
+cfg.dot.matrixWidth = 250; % in pixels
 cfg.dot.proportionKilledPerFrame = 0;
-cfg.dot.number = 10;
 
-cfg.timing.eventDuration = 100; % in seconds
+cfg.timing.eventDuration = 10; % in seconds
 
 relativeDensityContrast = dotMotionSimulation(cfg, thisEvent, nbEvents, doPlot);
