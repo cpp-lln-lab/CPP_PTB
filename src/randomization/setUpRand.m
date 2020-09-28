161be303799458810c843c8c11643e4c67933871
marcobarilari <marcobarilr@gmail.com>
2020-09-29 00:30:14 +0200

    update random type for octave

--- src/randomization/setUpRand.m
+++ src/randomization/setUpRand.m
@@ -2,12 +2,14 @@ function [] = setUpRand()
     % Set up the randomizers for uniform and normal distributions.
     % It is of great importance to do this before anything else!
 
+    % For an alternative from PTB see `ClockRandSeed`
+
     seed = sum(100 * clock);
 
     if isOctave
 
-        rand('state', seed);
-        randn('state', seed);
+        rand('twister', seed);
+        randn('twister', seed);
 
     else
 
