52212fe23f4723e0643fedec8676fbd5e5e05618
marcobarilari <marcobarilr@gmail.com>
2020-09-29 11:53:10 +0200

    add coverage for the most modern to very old matlab version

--- src/utils/setUpRand.m
+++ src/utils/setUpRand.m
@@ -1,7 +1,13 @@
-function [] = setUpRand()
-    % Set up the randomizers for uniform and normal distributions.
-    % It is of great importance to do this before anything else!
+% (C) Copyright 2010-2020 Sam Schwarzkopf
+% (C) Copyright 2020 CPP_PTB developers
 
+function setUpRand()
+    % setUpRand()
+    %
+    % Resets the seed of the random number generator. Will "adapt" depending on the matlab/octave
+    % version.
+    % It is of great importance to do this before anything else!
+    %
     % For an alternative from PTB see `ClockRandSeed`
 
     seed = sum(100 * clock);
@@ -13,8 +19,32 @@ function [] = setUpRand()
 
     else
 
-        RandStream.setGlobalStream(RandStream('mt19937ar', 'seed', seed));
+        try
+            % Use the reccomended method in modern Matlab
+            RandStream.setGlobalStream(RandStream('mt19937ar', 'seed', seed));
+            disp('Using modern randomizer...');
+        catch
 
+            try
+                % Use the recommended method in Matlab R2012a.
+                rng('shuffle');
+                disp('Using less modern randomizer...');
+            catch
+
+                try
+                    % Use worse methods for old versions of Matlab (e.g. 7.1.0.246 (R14) SP3).
+                    rand('twister', seed);
+                    randn('twister', seed);
+                    disp('Using outdated randomizer...');
+                catch
+                    % For very old Matlab versions these are the only methods you can use.
+                    % These are supposed to be flawed although you will probably not
+                    % notice any effect of this for most situations.
+                    rand('state', seed);
+                    randn('state', seed);
+                    disp('Using "flawed" randomizer...');
+                end
+            end
+        end
+        
     end
-
-end
