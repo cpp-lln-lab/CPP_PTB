323259e1f4e6a1272a278cfc89d0fc671f90539a
marcobarilari <marcobarilr@gmail.com>
2020-09-29 12:18:07 +0200

    put octave and matlab together

--- src/utils/setUpRand.m
+++ src/utils/setUpRand.m
@@ -12,38 +12,30 @@ function setUpRand()
 
     seed = sum(100 * clock);
 
-    if isOctave
-
-        rand('twister', seed);
-        randn('twister', seed);
-
-    else
+    try
+        % Use the reccomended method in modern Matlab
+        RandStream.setGlobalStream(RandStream('mt19937ar', 'seed', seed));
+        disp('Using modern randomizer...');
+    catch
 
         try
-            % Use the reccomended method in modern Matlab
-            RandStream.setGlobalStream(RandStream('mt19937ar', 'seed', seed));
-            disp('Using modern randomizer...');
+            % Use the recommended method in Matlab R2012a.
+            rng('shuffle');
+            disp('Using less modern randomizer...');
         catch
 
             try
-                % Use the recommended method in Matlab R2012a.
-                rng('shuffle');
-                disp('Using less modern randomizer...');
+                % Use worse methods for Octave or old versions of Matlab (e.g. 7.1.0.246 (R14) SP3).
+                rand('twister', seed);
+                randn('twister', seed);
+                disp('Using Octave or outdated randomizer...');
             catch
-
-                try
-                    % Use worse methods for old versions of Matlab (e.g. 7.1.0.246 (R14) SP3).
-                    rand('twister', seed);
-                    randn('twister', seed);
-                    disp('Using outdated randomizer...');
-                catch
-                    % For very old Matlab versions these are the only methods you can use.
-                    % These are supposed to be flawed although you will probably not
-                    % notice any effect of this for most situations.
-                    rand('state', seed);
-                    randn('state', seed);
-                    disp('Using "flawed" randomizer...');
-                end
+                % For very old Matlab versions these are the only methods you can use.
+                % These are supposed to be flawed although you will probably not
+                % notice any effect of this for most situations.
+                rand('state', seed);
+                randn('state', seed);
+                disp('Using "flawed" randomizer...');
             end
         end
 
