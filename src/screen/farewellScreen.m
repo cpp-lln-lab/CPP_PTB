f4804e85e62677c5ce6e059fb209409324429f72
Remi Gau <remi_gau@hotmail.com>
2020-10-17 13:37:40 +0200

    make farewellScreen more robust
    can run even when no cfg.mri field is present

--- src/screen/farewellScreen.m
+++ src/screen/farewellScreen.m
@@ -5,6 +5,8 @@ function farewellScreen(cfg)
     Screen('FillRect', cfg.screen.win, cfg.color.background, cfg.screen.winRect);
     DrawFormattedText(cfg.screen.win, 'Thank you!', 'center', 'center', cfg.text.color);
     Screen('Flip', cfg.screen.win);
-    WaitSecs(cfg.mri.repetitionTime * 2);
+    if isfield(cfg, 'mri')
+        WaitSecs(cfg.mri.repetitionTime * 2);
+    end
 
 end
