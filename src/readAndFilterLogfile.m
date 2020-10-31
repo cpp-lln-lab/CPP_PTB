ffd4154425aee3758ec0d258c965f808a81d6518
Remi Gau <remi_gau@hotmail.com>
2020-10-31 23:35:28 +0100

    partial fix help section readAndFilter

--- src/readAndFilterLogfile.m
+++ src/readAndFilterLogfile.m
@@ -2,8 +2,8 @@
 
 function [outputFiltered] = readAndFilterLogfile(columnName, filterBy, varargin)
     %
-    % It will display in the command window the content of the `output.tsv' filtered by one element
-    % of a target column. Dependecies: bids_matlab (from CPP_BIDS)
+    % It will display in the command window the content of the `output.tsv` filtered 
+    % by one element of a target column. Dependecies: bids_matlab (from CPP_BIDS)
     %
     % USAGE:
     %
@@ -13,14 +13,18 @@ function [outputFiltered] = readAndFilterLogfile(columnName, filterBy, varargin)
     %                    (e.g., for 'trigger' will be 'trial type')
     % :type columnName: string
     % :param filterBy: the content of the column you want to filter out. It can take just
-    %                   part of the content name (for example, you want to display the triggers and
-    %                   you have ``trigger_motion`` and ``trigger_static``, ``trigger``' as input
-    %                   will do)
+    %                  part of the content name (for example, you want to display the triggers and
+    %                  you have ``trigger_motion`` and ``trigger_static``, ``trigger`` as input
+    %                  will do)
     % :type filterBy: string
-    % :param varargin: either ``cfg`` (to display the last run output) or the file path as string
+    % :param varargin: either ``cfg`` (to display the last run output) or 
+    %                  the file path as string
     %
-    % :returns: - outputFiltered: dataset with only the specified content, to see it in the command
-    %             window use display(outputFiltered)
+    % :returns: - :outputFiltered: dataset with only the specified content
+    %             to see it in the command window use display(outputFiltered)
+    %
+    %
+    
 
     % Checke if input is cfg or the file path
     if ischar(varargin{1})
