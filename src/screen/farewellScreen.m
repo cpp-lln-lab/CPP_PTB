77896d8899e68dae437bacacc86af7fe70575c31
Remi Gau <remi_gau@hotmail.com>
2022-08-27 22:44:56 +0200

    add optional message to farewell screen

--- src/screen/farewellScreen.m
+++ src/screen/farewellScreen.m
@@ -1,9 +1,13 @@
-function farewellScreen(cfg)
+function farewellScreen(cfg, message)
     %
     % (C) Copyright 2020 CPP_PTB developers
 
+    if nargin < 2
+        message = 'Thank you for participating in this experiment.';
+    end
+
     Screen('FillRect', cfg.screen.win, cfg.color.background, cfg.screen.winRect);
-    DrawFormattedText(cfg.screen.win, 'Thank you!', 'center', 'center', cfg.text.color);
+    DrawFormattedText(cfg.screen.win, message, 'center', 'center', cfg.text.color);
     Screen('Flip', cfg.screen.win);
     if isfield(cfg, 'mri')
         WaitSecs(cfg.mri.repetitionTime * 2);
